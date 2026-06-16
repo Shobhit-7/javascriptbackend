a73dfd6e203585e4b9c978de7ae2f67290b89b9f
shobhitshukla <engshobhitshukla@gmail.com>
2026-06-16 16:41:48 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,83 @@
+ import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // FIRST METHOD
+
+// import express from "express";
+
+// const app = express();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(
+//             `${process.env.MONGODB_URL}/${DB_NAME}`
+//         );
+
+//         app.on("error", (error) => {
+//             console.log("Error:", error);
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`APP IS LISTENING ON PORT ${process.env.PORT}`);
+//         });
+
+//     } catch (error) {
+//         console.error("Error:", error);
+//         throw error;
+//     }
+// })();
